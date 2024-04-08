ca86ba73376b1ad03aa5d031f2f315a1bccb5365
NickolayD <145503799+NickolayD@users.noreply.github.com>
2024-04-09 00:08:12 +0300

    Update app.py

--- application/app.py
+++ application/app.py
@@ -1,11 +1,13 @@
 from config import model, veg_dict
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 import uvicorn
 
+
 class MyArr(BaseModel):
     array: List[float]
+
 
 app = FastAPI()
 
@@ -14,12 +16,17 @@ app = FastAPI()
 async def homepage():
     return {"Status": "Everything is OK!"}
 
+
 @app.post('/predict')
-async def make_prediction(obj: MyArr):
+async def predict(obj: MyArr):
+    if len(obj.array) != 15488:
+        raise HTTPException(
+            status_code=1001,
+            detail="Wrong data size."
+        )
     prediction = model.predict([obj.array])
-    return {
-        "Vegetable": veg_dict[prediction[0]],
-        }
-    
+    return {"Vegetable": veg_dict[prediction[0]]}
+
+
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=5001)
